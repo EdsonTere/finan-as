2457b2ea59a68d4c0d0f3d4bd96ac0d844d5730c
EdsonTere <edsonveve@gmail.com>
2026-02-22 00:12:55 -0300

    Fix missing useEffect import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react'
+import { useMemo, useState, useEffect } from 'react'
 import { AuthProvider, useAuth } from './contexts/AuthContext'
 import { FinanceProvider, useFinance } from './contexts/FinanceContext'
 import { MainLayout } from './components/layout/MainLayout'
